3507a22e8360a57198873a17b82d0372db5cb357
Khanetski Yayhen <xats89@gmail.com>
2024-01-18 09:59:10 +0300

    fix(04-copy-directory): done with function

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,22 +3,27 @@ const path = require('path');
 
 const folderPath = path.join(__dirname, 'secret-folder');
 
-fs.readdir(folderPath, { withFileTypes: true})
-  .then(direntData => {
+fs.readdir(folderPath, { withFileTypes: true })
+  .then((direntData) => {
     direntData.forEach((direntElement) => {
-      if(direntElement.isFile()) {
+      if (direntElement.isFile()) {
         const filePath = path.join(folderPath, direntElement.name);
         const fileInfo = path.parse(filePath);
 
         fs.stat(filePath)
-          .then(fileStat => {
-            const result = `${fileInfo.name} - ${fileInfo.ext.replace('.', '')} - ${fileStat.size * 0.001}kb`
-            console.log(result)
-          }).catch(err => {
+          .then((fileStat) => {
+            const result = `${fileInfo.name} - ${fileInfo.ext.replace(
+              '.',
+              '',
+            )} - ${fileStat.size * 0.001}kb`;
+            console.log(result);
+          })
+          .catch((err) => {
             console.log(err);
           });
-      };
+      }
     });
-  }).catch(err => {
+  })
+  .catch((err) => {
     console.log(err);
   });
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,27 +4,37 @@ const path = require('path');
 const filesFolder = path.join(__dirname, 'files');
 const copyFilesFolder = path.join(__dirname, 'files-copy');
 
-// исправить: если есть папка с копией - то её сразу удалить, а потом создать заново
-fs.stat(copyFilesFolder, (err) => {
-  if (err) {
-    fs.mkdir(copyFilesFolder, { recursive: true }, (err) => {
-      if (err) console.log(err);
+function delCopyFiles() {
+  fs.readdir(copyFilesFolder, (err, data) => {
+    if (err) return;
+    data.forEach((file) => {
+      fs.unlink(path.join(copyFilesFolder, file), (err) => {
+        if (err) throw err;
+      });
     });
-  } else {
-    fs.rm(copyFilesFolder, { recursive: true }, (err) => {
-      if (err) console.log(err);
-    });
-  }
-});
-
-fs.readdir(filesFolder, (err, data) => {
-  data.forEach((file) => {
-    fs.copyFile(
-      path.join(filesFolder, file),
-      path.join(copyFilesFolder, file),
-      (err) => {
-        if (err) console.log(err);
-      },
-    );
   });
-});
+}
+
+function createCopyFolder() {
+  fs.mkdir(copyFilesFolder, { recursive: true }, (err) => {
+    if (err) console.log(err);
+  });
+}
+
+function copyFiles() {
+  fs.readdir(filesFolder, (err, data) => {
+    data.forEach((file) => {
+      fs.copyFile(
+        path.join(filesFolder, file),
+        path.join(copyFilesFolder, file),
+        (err) => {
+          if (err) console.log(err);
+        },
+      );
+    });
+  });
+}
+
+delCopyFiles();
+createCopyFolder();
+copyFiles();
